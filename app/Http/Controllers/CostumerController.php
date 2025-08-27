f66b5727275d0407132345158047126e64365963
Salmanziz21 <saziz2455@gmail.com>
2025-08-27 09:40:19 +0700

    menambah fitur role,menambah user data,memperbaiki print,dan memperbaiki gambar product

---
+++ app/Http/Controllers/CostumerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+
+class CostumerController extends Controller
+{
+    public function index()
+    {
+        return view('Welcome');
+    }
+
+}
