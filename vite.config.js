209a117a5dc4c51f12ab88ec00510f15f039581d
rifqihytam <kambingjawa009@gmail.com>
2025-10-14 17:07:38 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,18 @@
-import { defineConfig } from 'vite';
+import {
+  defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    tailwindcss(),
+      laravel({
+          input: ['resources/css/app.css', 'resources/js/app.js'],
+          refresh: true,
+      }),
+      tailwindcss(),
   ],
   server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    cors: {
-      origin: '*',
-    },
-    hmr: {
-      host: '192.168.0.196',
-      port: 5173,             
-    },
+      cors: true,
   },
 });
